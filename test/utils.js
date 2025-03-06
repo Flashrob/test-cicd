1d847b0d3d7bbc9ecaec3eced16001b62ffe682d
PauPauRob <robert.kolsek@foodpanda.com>
2025-03-06 20:06:51 +0800

    Update test

--- test/utils.js
+++ test/utils.js
@@ -5,7 +5,7 @@ describe("Utils", () => {
   describe("Add", () => {
     it("Adds 2 of the same number", () => {
       const result = add(1, 1);
-      expect(result).to.equal(3);
+      expect(result).to.equal(4);
     });
 
     it("Adds 2 different numbers", () => {
@@ -24,3 +24,21 @@ describe("Utils", () => {
     });
   });
 });
+
+// CREATE BRANCH AND CHECK OUT TO IT
+// git branch <name> + git checkout <name>
+// git checkout -b <name>
+
+// MAKE A CHANGE, ADD IT, COMMIT IT, PUSH IT
+// git add .
+// git commit -m "commit message"
+// git push
+
+// SET THE ORIGIN
+// set the origin, copy paste from CLI
+
+// CREATE THE PR
+// click on link to create PR
+
+// CREATE PULL REQUEST, DONT FORGET TO POINT TO YOUR OWN REPO
+// point PR to your own test-cicd repo
